6cffbf0f965514db2b15c4dd359c2eb24f8ecac9
unknown <garudaalfayadh@gmail.com>
2024-03-07 19:56:42 +0700

    daftarlokasi

--- app/Http/Controllers/DaftarLokasiController.php
+++ app/Http/Controllers/DaftarLokasiController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class DaftarLokasiController extends Controller
 {
-    //
+    public function index()
+    {
+       // $data = [
+       //     'nama' => 'Doraemon',
+       //     'pekerjaan' => 'Developer',
+       // ];
+    
+       // return view('home')->with($data);
+    $nama = "Nobita";
+    $pekerjaan = "Student";
+    return view('daftarLokasi', compact('nama','pekerjaan'));
+ }
+ public function contact(){
+    return view('contact');
+ }
 }
