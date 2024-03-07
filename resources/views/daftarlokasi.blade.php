4a1e83242dba0012de247519b626d6dd954eff0f
unknown <garudaalfayadh@gmail.com>
2024-03-07 19:49:33 +0700

    add listlokasi

---
+++ resources/views/daftarlokasi.blade.php
@@ -0,0 +1,7 @@
+
+<div>
+        <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+    Selamat datang di login {{$nama}}<br>
+    Anda adalah seorang {{$pekerjaan}}
+</div>   <!-- It is never too late to be what you might have been. - George Eliot -->
+
