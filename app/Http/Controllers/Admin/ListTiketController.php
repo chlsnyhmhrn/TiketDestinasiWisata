e5a2077ee558f2e0ec15d82b8e1b75382b438496
Zahrul Romadhon <zahrulromadhon8@gmail.com>
2024-06-15 21:28:51 +0700

    buat fungsi crud admin/list_tiket

--- app/Http/Controllers/Admin/ListTiketController.php
+++ app/Http/Controllers/Admin/ListTiketController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tiket;
 use Illuminate\Http\Request;
 
 class ListTiketController extends Controller
 {
     public function index()
-    {
-        return view('admin.list_tiket');
-    }
+{
+    $tikets = Tiket::with(['destinasi', 'user'])->get();
+    return view('admin.list_tiket', compact('tikets'));
+}
 }
