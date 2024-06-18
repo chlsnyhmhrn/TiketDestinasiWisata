dc213d092e76185ed16b97258af25a94c35c0004
Zacky <zacky20okto@gmail.com>
2024-06-18 20:40:45 +0700

    dashboard penjual

--- app/Http/Controllers/Penjual/DetailDestinasiController.php
+++ app/Http/Controllers/Penjual/DetailDestinasiController.php
@@ -14,4 +14,20 @@ class DetailDestinasiController extends Controller
 
         return view('penjual.detail_destinasi', compact('destinasi'));
     }
+
+    public function editDetail(Request $request ,$id_destinasi){
+        $destinasi = Destinasi::where('id_destinasi', $id_destinasi)->firstOrFail();
+
+        $destinasi->update([
+            'id_kategori' => $request->input('id_kategori'),
+            'nama_destinasi'=> $request->input('nama_destinasi'),
+            'lokasi'=> $request->input('lokasi'),
+            'deskripsi'=> $request->input('deskripsi'),
+            'harta'=> $request->input('harga'),
+            'jam_buka'=> $request->input('jam_buka'),
+            'jam_tutup'=> $request->input('jam_tutup')
+        ]);
+        
+        
+    }
 }
